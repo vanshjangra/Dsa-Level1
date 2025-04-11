9c26c3a3d84a931e741b546da9ba74fa7a0aa8cf
vanshjangra <vanshjangra586@gmail.com>
2025-04-11 14:51:45 +0530

    print z

---
+++ gettingstarted/Main.java
@@ -0,0 +1,13 @@
+package gettingstarted;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+
+        System.out.println("*\n**\n***\n****\n*****");
+    }
+}
---
+++ gettingstarted/QuestionPrintZ.java
@@ -0,0 +1,11 @@
+package gettingstarted;
+
+public class QuestionPrintZ {
+    public static void main(String[] args) {
+        System.out.println("*****");
+        System.out.println("   *");
+        System.out.println("  *");
+        System.out.println(" *");
+        System.out.println("*****");
+    }
+}
