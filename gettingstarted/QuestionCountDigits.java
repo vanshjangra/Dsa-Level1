e4a6aa062005a41992d3148c732643f7e1d6ae48
vanshjangra <vanshjangra586@gmail.com>
2025-04-12 15:09:18 +0530

    count digits

---
+++ gettingstarted/QuestionCountDigits.java
@@ -0,0 +1,17 @@
+package gettingstarted;
+
+import java.util.Scanner;
+
+public class QuestionCountDigits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int dig = 0;
+        while (n != 0) {
+            n = n / 10;
+            dig++;
+        }
+        System.out.println(dig);
+    }
+}
