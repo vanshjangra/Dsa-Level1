471f2a96fa98a333fdbc34b5ff572ca6861564aa
vanshjangra <vanshjangra586@gmail.com>
2025-04-15 19:32:02 +0530

    pattern 20

---
+++ patterns/QuestionPattern20.java
@@ -0,0 +1,25 @@
+package patterns;
+
+import java.util.Scanner;
+
+public class QuestionPattern20 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n; j++){
+                if(j == 1 || j == n){
+                    System.out.print("*\t");
+                }
+                else if(i > n / 2 && (i == j || i + j == n + 1)){
+                    System.out.print("*\t");
+                }
+                else{
+                    System.out.print("\t");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
