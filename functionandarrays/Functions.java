844cdb4a2ebd8d0486afd71dcbff69730a6ffb11
vanshjangra <vanshjangra586@gmail.com>
2025-04-16 11:32:18 +0530

    digits frequency

---
+++ functionandarrays/Functions.java
@@ -0,0 +1,31 @@
+package functionandarrays;
+
+import java.util.Scanner;
+
+public class Functions {
+
+    public static void display(int n, int r, int npr){
+        System.out.println(n + "P" + r + " = " + npr);
+    }
+
+    public static int fact(int x){
+        int rv = 1;
+
+        for(int i = 1; i <= x; i++){
+            rv = rv * i;
+        }
+
+        return rv;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int r = sc.nextInt();
+
+        int nfact = fact(n);
+        int nmrfact = fact(n - r);
+
+        int npr = nfact / nmrfact;
+        display(n, r, npr);
+    }
+}
